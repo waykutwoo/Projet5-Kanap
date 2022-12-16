d344effd6408f6b6032934427596da897073cf52
waykutwoo <aykutkokluk@gmail.com>
2022-12-16 16:08:52 +0300

    Projet 5 Kanap
    modif Readme

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -11,11 +11,13 @@
 // la propriété textContent de l’objet orderIdSpan pour remplacer son contenu actuel par l’ID de commande stocké dans la variable orderIdorderId
 // ce code affiche l’ID de commande de la page courante dans un élément HTML avec l’ID  orderId
 // création de l'objet response
+
+
 const currentPageUrl = document.location.href;
 const url = new URL(currentPageUrl);
 const orderId = url.searchParams.get("order");
 const orderIdSpan = document.getElementById("orderId");
-const response = new URL("http://localhost:3000/api/products");
+const response = new URL("http://localhost:3000");
 console.log(response);
 orderIdSpan.textContent = `${orderId}`;
 
